da27373ca95f659c676fc3bfaff8d11ce89f96d5
Jason Baik <baikjs87@gmail.com>
2022-08-24 23:14:49 -0400

    Complete

--- src/reducer.js
+++ src/reducer.js
@@ -7,7 +7,18 @@ function changeState(state, action) {
   }
 }
 
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+}
+
+function render() {
+  document.body.textContent = state.count;
+}
+
 let state = { count: 0 };
 let action = { type: "counter/increment" };
 
 changeState(state, action);
+dispatch(action);
+render();
